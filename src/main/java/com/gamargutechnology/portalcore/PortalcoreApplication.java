c30867739a37771da489af30de7bc3d5865019a0
ibrahimballamsf <ibrahim.balla@brussels.msf.org>
2023-05-23 10:47:53 +0200

    First Commit

---
+++ src/main/java/com/gamargutechnology/portalcore/PortalcoreApplication.java
@@ -0,0 +1,13 @@
+package com.gamargutechnology.portalcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalcoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamargutechnology/portalcore/PortalcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamargutechnology.portalcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
